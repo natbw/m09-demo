79bf5446ea61db54892a31923cd6b332cb993d4c
Brian Natale <bmw8pt@virginia.edu>
2024-10-29 20:18:13 -0400

    demo module

---
+++ demo/jabber.py
@@ -0,0 +1,3 @@
+
+def tell_something():
+    print('something')
---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+    name='Demo',
+    version='0.1',
+    description='A simple demonstration package',
+    license='MIT',
+    packages=['demo']
+)
